0f50400ff85adc58f597dd7b27a65e1b45ec125b
Simone Ascione <104779923+asciosh@users.noreply.github.com>
2024-11-09 19:52:07 +0100

    update

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ import numpy as np
 app = Flask(__name__)
 
 # Definisci il percorso per il modello salvato
-MODEL_PATH = '/model.pkl'
+MODEL_PATH = 'model.pkl'  # Assicurati che il modello sia nel percorso giusto
 
 # Carica il modello all'avvio dell'app
+model = None
 try:
     model = joblib.load(MODEL_PATH)
     print("Modello caricato con successo.")
@@ -17,9 +18,16 @@ except Exception as e:
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    if model is None:
+        return jsonify({'error': 'Il modello non è stato caricato correttamente. Contatta il supporto.'}), 500
+
     try:
         # Ricevi i dati dalla richiesta POST
         data = request.get_json()  # Supponiamo che i dati siano in formato JSON
+        
+        # Verifica che i dati siano nel formato corretto
+        if 'features' not in data:
+            return jsonify({'error': "'features' manca nei dati"}), 400
         
         # Esegui la previsione con il modello
         prediction = model.predict(np.array(data['features']).reshape(1, -1))
